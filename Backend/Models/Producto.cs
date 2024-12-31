f36b181ade607875a0432a25214482fd408c6753
JuanFullstack <omega3031@gmail.com>
2024-12-31 17:15:08 -0300

    Agregar archivos de proyecto.

---
+++ Backend/Models/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Producto
+    {
+        // Identificador único del producto
+        public int Id { get; set; }
+
+
+        // Nombre del producto, con una longitud máxima de 50 caracteres
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string Nombre { get; set; } = null!;
+
+
+        // Descripción del producto, con posibilidad de texto multilínea y una longitud máxima de 500 caracteres
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string Descripcion { get; set; } = null!;
+
+
+        // Precio del producto, almacenado como decimal con 18 dígitos en total y 2 decimales
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Precio { get; set; }
+    }
+
+}
---
+++ Backend/Models/ProductosCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class ProductosCRUDContext : DbContext
+    {
+        public ProductosCRUDContext(DbContextOptions<ProductosCRUDContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Producto>().HasIndex(c => c.Nombre).IsUnique();
+        }
+    }
+}
